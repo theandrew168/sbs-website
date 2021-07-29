a02f0b619345569575c65b8fde6399784611d597
theandrew168 <andrew@shallowbrooksoftware.com>
2021-07-29 00:15:14 -0500

    Remove a line

--- mail/sendgrid.go
+++ mail/sendgrid.go
@@ -21,7 +21,6 @@ func NewSendGridMailer(sendGridAPIKey string) *sendGridMailer {
 func (m *sendGridMailer) SendMail(fromName, fromEmail, toName, toEmail, subject, body string) error {
 	from := mail.NewEmail(fromName, fromEmail)
 	to := mail.NewEmail(toName, toEmail)
-
 	message := mail.NewSingleEmailPlainText(from, subject, to, body)
 
 	client := sendgrid.NewSendClient(m.sendGridAPIKey)
